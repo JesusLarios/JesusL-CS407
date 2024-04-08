fce5cce71108914bda1d57c9ee12015487a900e1
Jesus Larios <jlarios20@mail.wou.edu>
2024-04-07 17:10:01 -0700

    Created two buttons for to toggle animation and wireframe of cone

--- WorkShowcaseApp/src/routes/assignment1/+page.svelte
+++ WorkShowcaseApp/src/routes/assignment1/+page.svelte
@@ -9,10 +9,39 @@
         Scene,
         WebGLRenderer,
     } from 'three';
+
+    let showWireframe = false;
+    let showAnimation = false;
+    let cone;
+    let renderer;
+    let scene;
+    let camera;
+
+    function toggleWireframe() {
+            showWireframe = !showWireframe;
+            cone.material.wireframe = showWireframe;
+            renderer.render(scene, camera);
+        }
+
+    function toggleAnimation() {
+        showAnimation = !showAnimation;
+        if (showAnimation) {
+            animateCone();
+        }
+    }
+
+    function animateCone() {
+        if (showAnimation) {
+            cone.rotation.x -= 0.01;
+            cone.rotation.y += 0.008;
+            renderer.render(scene, camera);
+            requestAnimationFrame(animateCone);
+        }
+    }
     
     onMount(() => {
         //Create a scene
-        const scene = new Scene();
+        scene = new Scene();
         scene.background = new Color('dimgray');
 
         // Create a camera
@@ -20,21 +49,21 @@
         const aspect = window.innerWidth / window.innerHeight;
         const near = 0.1; // the near clipping plane
         const far = 200; // the far clipping plane
-        const camera = new PerspectiveCamera(fov, aspect, near, far);
+        camera = new PerspectiveCamera(fov, aspect, near, far);
 
         // Adjust camera to be outside the center
         camera.position.set(0, 0, 100);
 
         // Create a Mesh using a geometry and material
         const geometry = new ConeGeometry(5, 10, 8);
-        const material = new MeshBasicMaterial({color: 0xff9705, wireframe: true});
-        const cone = new Mesh(geometry, material);
+        const material = new MeshBasicMaterial({color: 0xff9705});
+        cone = new Mesh(geometry, material);
 
         // Add the mesh to the scene
         scene.add(cone);
         
         // Create a renderer
-        const renderer = new WebGLRenderer();
+        renderer = new WebGLRenderer();
         renderer.setSize(window.innerWidth/2, window.innerHeight/2);
 
         // Add the automatically created <canvas> element to the page
@@ -43,26 +72,22 @@
         
         // Render, or 'create a still image', of the scene
         renderer.render(scene, camera);
-
-        // Continuosly rotate cone to render
-        function animateCone() {
-        cone.rotation.x -= 0.01;
-        cone.rotation.y += 0.008;
-        renderer.render(scene, camera);
-        requestAnimationFrame(animateCone);
-      }
-      animateCone();
     });
-    
-
-    
 </script>
     
 <div id="scene-container">
     <h1>Hello World! using three.js</h1>
 </div>
 
+<button on:click={toggleWireframe}>
+    {showWireframe ? 'Hide' :' Show'}
+    wireframe
+</button>
 
+<button on:click={toggleAnimation}>
+    {showAnimation ? 'Stop' :' Start'}
+    animation
+</button>
 
 <style>
 
