a1e527f124e069d031fb198db1421073f8c97eb5
Jesus Larios <jlarios20@mail.wou.edu>
2024-04-12 22:34:21 -0700

    Changed position and size of light

--- WorkShowcaseApp/src/routes/assignment3/src/World/components/lights.js
+++ WorkShowcaseApp/src/routes/assignment3/src/World/components/lights.js
@@ -2,10 +2,10 @@ import { DirectionalLight } from 'three';
 
 function createLights() {
   // Create a directional light
-  const light = new DirectionalLight('white', 8);
+  const light = new DirectionalLight('white', 13);
 
   // move the light right, up, and towards us
-  light.position.set(10, 10, 10);
+  light.position.set(20, 31, 31);
 
   return light;
 }
