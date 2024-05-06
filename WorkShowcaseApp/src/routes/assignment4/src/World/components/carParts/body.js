dbd4d7ca909a70531232f1bc3cc27d1aa08ca009
Jesus Larios <jlarios20@mail.wou.edu>
2024-05-05 19:23:54 -0700

    Changed coordinate direction of upper and lower body

--- WorkShowcaseApp/src/routes/assignment4/src/World/components/carParts/body.js
+++ WorkShowcaseApp/src/routes/assignment4/src/World/components/carParts/body.js
@@ -9,18 +9,18 @@ function createBody() {
     const material1 = new MeshStandardMaterial();
     const body = new Mesh(geometry1, material1);
 
-    const geometry2 = new BoxGeometry(35, 7, 15);
+    const geometry2 = new BoxGeometry(15, 7, 35);
     const material2 = new MeshStandardMaterial({ color: 'FireBrick' });
     const lowerBody = new Mesh(geometry2, material2);
 
     body.add(lowerBody);
 
-    const geometry3 = new BoxGeometry(15, 7, 13);
+    const geometry3 = new BoxGeometry(13, 7, 15);
     const material3 = new MeshStandardMaterial({ color: 'white' });
     const upperBody = new Mesh(geometry3, material3);
 
     body.add(upperBody);
-    upperBody.position.set(4, 5, 0);
+    upperBody.position.set(0, 5, 4);
 
     // this method will be called once per frame
     body.tick = (delta) => {
