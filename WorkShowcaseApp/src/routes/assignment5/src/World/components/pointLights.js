c09cfd8b1a8754da9ed7f59833dfaae2c09cbd4e
Jesus Larios <jlarios20@mail.wou.edu>
2024-05-08 22:39:28 -0700

    Moved point light and increased brightness

--- WorkShowcaseApp/src/routes/assignment5/src/World/components/pointLights.js
+++ WorkShowcaseApp/src/routes/assignment5/src/World/components/pointLights.js
@@ -1,9 +1,9 @@
 import { PointLight } from 'three';
 
 function createPointLights() {
-    const light = new PointLight(0xff00ff, 1000, 200);
+    const light = new PointLight(0xff00ff, 3000, 200);
 
-    light.position.set(-10, 10, -10);
+    light.position.set(-10, -10, -10);
 
     return light;
 }
