b9e966bee9546054a38a8ba80ff88a4d99e3aa17
Jesus Larios <jlarios20@mail.wou.edu>
2024-05-08 22:32:35 -0700

    Changed controls to target closer to the center of the diamond

--- WorkShowcaseApp/src/routes/assignment5/src/World/systems/controls.js
+++ WorkShowcaseApp/src/routes/assignment5/src/World/systems/controls.js
@@ -2,6 +2,7 @@ import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
 
 function createControls(camera, canvas) {
     const controls = new OrbitControls(camera, canvas);
+    controls.target.set(0, 0, -1.5);
 
     // damping and auto rotation require
     // the controls to be updated each frame
