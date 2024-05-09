c0a72549c250a8ab4b3fb36cc9d730c2fbd8e2d8
Jesus Larios <jlarios20@mail.wou.edu>
2024-05-08 22:15:26 -0700

    Lowerd ambient light intensity

--- WorkShowcaseApp/src/routes/assignment5/src/World/components/ambientLights.js
+++ WorkShowcaseApp/src/routes/assignment5/src/World/components/ambientLights.js
@@ -1,7 +1,7 @@
 import { AmbientLight } from 'three';
 
 function createAmbientLights() {
-  const ambientLight = new AmbientLight(0xffffff, 1.5);
+  const ambientLight = new AmbientLight(0xffffff, 4);
 
   return ambientLight;
 }
