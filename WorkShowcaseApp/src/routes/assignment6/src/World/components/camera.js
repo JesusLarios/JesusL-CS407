909dbcd63a5a13d09704400ed1b6c11408256372
Jesus Larios <jlarios20@mail.wou.edu>
2024-05-18 00:36:54 -0700

    Increased viewing range and moved camera further from center

--- WorkShowcaseApp/src/routes/assignment6/src/World/components/camera.js
+++ WorkShowcaseApp/src/routes/assignment6/src/World/components/camera.js
@@ -5,11 +5,11 @@ function createCamera() {
     35, // fov = Field Of View
     1, // aspect ratio (dummy value)
     0.1, // near clipping plane
-    200, // far clipping plane
+    300, // far clipping plane
   );
 
   // move the camera back so we can view the scene
-  camera.position.set(0, 0, 15);
+  camera.position.set(0, 5, 25);
 
   return camera;
 }
