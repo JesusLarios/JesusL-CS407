d1718b42bdcae5b737b7e1d61d976eb0d0043a80
Jesus Larios <jlarios20@mail.wou.edu>
2024-05-17 22:21:37 -0700

    Added ability for helicopter to turn and move up or down

--- WorkShowcaseApp/src/routes/assignment6/src/World/components/helicopter/setupModel.js
+++ WorkShowcaseApp/src/routes/assignment6/src/World/components/helicopter/setupModel.js
@@ -1,17 +1,68 @@
-import { AnimationMixer } from 'three';
+import { AnimationMixer, Vector3 } from 'three';
 
 function setupModel(data) {
-  const model = data.scene.children[0];
-  const clip = data.animations[0];
+    const model = data.scene.children[0];
+    const clip = data.animations[0];
 
-  const mixer = new AnimationMixer(model);
-  const action = mixer.clipAction(clip);
-  action.play();
+    const mixer = new AnimationMixer(model);
+    const action = mixer.clipAction(clip);
+    action.play();
 
-  model.tick = (delta) => mixer.update(delta);
+    let driveDirection = 'stop';
+    let driveSpeed = 8;
 
-  return model;
-  }
-  
-  export { setupModel };
+    let turnDirection = 'stop';
+    let turnSpeed = Math.PI/360 * 90;
+
+    let liftDirection = 'stop';
+    let liftSpeed = 3;
+
+    model.tick = (delta) => {
+        mixer.update(delta);
+
+        const forwardDirection = new Vector3(0, 1, 0).applyQuaternion(model.quaternion);
+
+        if (driveDirection !== 'stop') {
+            if (driveDirection === 'forward') {
+                model.position.add(forwardDirection.clone().multiplyScalar(-driveSpeed * delta));
+            } else if (driveDirection === 'backward') {
+                model.position.add(forwardDirection.clone().multiplyScalar(driveSpeed * delta));
+            }
+        }
+
+        if (turnDirection !== 'stop') {
+            if (turnDirection === 'left') {
+                model.rotation.z += turnSpeed * delta;
+            }
+            else if (turnDirection === 'right') {
+                model.rotation.z -= turnSpeed * delta;
+            }
+        }
+
+        if (liftDirection !== 'stop') {
+            if (liftDirection === 'up') {
+                model.position.y += liftSpeed * delta;
+            }
+            else if (liftDirection === 'down') {
+                model.position.y -= liftSpeed * delta;
+            }
+        }
+    }
+
+    model.drive = (direction) => {
+        driveDirection = direction;
+    }
+
+    model.turn = (direction) => {
+        turnDirection = direction;
+    }
+
+    model.lift = (direction) => {
+        liftDirection = direction;
+    }
+
+    return model;
+    }
+    
+    export { setupModel };
   
