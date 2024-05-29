cce06f69ec7925bb9c44372a9f15c7ff0657d996
Jesus Larios <jlarios20@mail.wou.edu>
2024-05-29 00:25:24 -0700

    Added uniform for color speed and collapse

--- WorkShowcaseApp/src/routes/assignment7/src/World/components/torus.js
+++ WorkShowcaseApp/src/routes/assignment7/src/World/components/torus.js
@@ -8,28 +8,37 @@ import {
     const geometry = new TorusGeometry( 8, 3, 16, 100 );
 
     const vertexShader = `
+    uniform bool collapse;
         out vec3 vPosition;
   
         void main() {
-            vPosition = position;
-            gl_Position = projectionMatrix * modelViewMatrix * vec4(position, 1.0);
+            vec3 newPosition = position;
+            if (collapse) {
+                if (position.x > 0.0 && position.y > 0.0) {
+                    newPosition = vec3(0.0, 0.0, position.z);
+                } else if (position.x < 0.0 && position.y < 0.0) {
+                    newPosition = vec3(0.0, 0.0, position.z);
+                }
+            }
+            vPosition = newPosition;
+
+            gl_Position = projectionMatrix * modelViewMatrix * vec4(newPosition, 1.0);
         }
     `;
   
     const fragmentShader = `
+        uniform float speed;
         uniform float time;
         in vec3 vPosition;
   
         void main() {
+            
             vec3 color1 = vec3(1.0, 0.0, 0.0);
             vec3 color2 = vec3(0.0, 1.0, 0.0);
-            
-            //if (vPosition.x >= 0.0 && vPosition.y >= 0.0) {
-            if (time > 1.0) {
-                gl_FragColor = vec4(color1, 1.0);
-            } else {
-                gl_FragColor = vec4(color2, 1.0);
-            }
+                
+            float mixFactor = abs(sin(time/speed));
+            vec3 color = mix(color1, color2, mixFactor);
+            gl_FragColor = vec4(color, 1.0);
         }
     `;
   
@@ -37,11 +46,23 @@ import {
         vertexShader,
         fragmentShader,
         uniforms: {
-            time: { value: 0 },
+            //color1: { value: new  Vector3(1.0, 0.0, 0.0) },
+            //color2: { value: new  Vector3(0.0, 1.0, 0.0) },
+            collapse: { value: false},
+            speed: { value: 5.0 },
+            time: { value: 0.0 },
         },
     });
   
     const torus = new Mesh(geometry, material);
+
+    torus.setCollapse = (collapse) => {
+        torus.material.uniforms.collapse.value = collapse;
+    }
+
+    torus.setSpeed = (speed) => {
+        torus.material.uniforms.speed.value = speed;
+    };
 
     torus.tick = (delta) => {
         torus.material.uniforms.time.value += delta;
