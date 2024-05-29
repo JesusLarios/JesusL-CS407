77d7a301282d354c879a50afc01b489a9561ede6
Jesus Larios <jlarios20@mail.wou.edu>
2024-05-28 18:31:52 -0700

    Plane material uses shaders located in external files

--- WorkShowcaseApp/src/routes/assignment7/src/World/components/plane.js
+++ WorkShowcaseApp/src/routes/assignment7/src/World/components/plane.js
@@ -1,19 +1,27 @@
 import {
-    PlaneGeometry,
-    Mesh,
-    MeshStandardMaterial,
-    DoubleSide,
-  } from 'three';
-  
-  function createPlane() {
-    const geometry = new PlaneGeometry(100, 100);
-    const material = new MeshStandardMaterial({ color: 0x00ff44, side: DoubleSide });
-    const plane = new Mesh(geometry, material);
-  
-    plane.rotation.set(-0.5 * Math.PI, 0, 0);
-  
-    return plane;
-  }
-  
-  export { createPlane };
-  
+  PlaneGeometry,
+  Mesh,
+  ShaderMaterial,
+  DoubleSide,
+} from 'three';
+
+// Helper function to load shaders
+async function loadShader(path) {
+  const response = await fetch(path);
+  return await response.text();
+}
+
+async function createPlane() {
+  const geometry = new PlaneGeometry(100, 100);
+
+  const vertexShader = await loadShader('./shaders/planeVS.glsl');
+  const fragmentShader = await loadShader('./shaders/planeFS.glsl');
+
+  const material = new ShaderMaterial({ vertexShader, fragmentShader, side: DoubleSide });
+
+  const plane = new Mesh(geometry, material);
+
+  return plane;
+}
+
+export { createPlane };
