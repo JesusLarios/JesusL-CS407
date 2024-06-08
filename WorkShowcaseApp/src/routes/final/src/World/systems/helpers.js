d288314238c8346876f0664b30aa5a94ad67c052
Jesus Larios <jlarios20@mail.wou.edu>
2024-06-07 20:39:25 -0700

    Added functions to create a grid helper and other light helpers

--- WorkShowcaseApp/src/routes/final/src/World/systems/helpers.js
+++ WorkShowcaseApp/src/routes/final/src/World/systems/helpers.js
@@ -1,8 +1,28 @@
-import { AxesHelper } from 'three';
+import { AxesHelper, GridHelper, DirectionalLightHelper, PointLightHelper, SpotLightHelper  } from 'three';
 
 function createAxesHelper() {
   const helper = new AxesHelper(3);
   return helper;
 }
 
-export { createAxesHelper };
+function createGridHelper() {
+  const helper = new GridHelper(50, 50);
+  return helper;
+}
+
+function createDirectionalLightHelper(light) {
+  const helper = new DirectionalLightHelper(light);
+  return helper;
+}
+
+function createPointLightHelper(light) {
+  const helper = new PointLightHelper(light);
+  return helper;
+}
+
+function createSpotLightHelper(light) {
+  const helper = new SpotLightHelper(light);
+  return helper;
+}
+
+export { createAxesHelper, createGridHelper, createDirectionalLightHelper, createPointLightHelper, createSpotLightHelper };
