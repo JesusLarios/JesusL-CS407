15112a78920efcd830a5a77df9d1a06418f93b06
Jesus Larios <jlarios20@mail.wou.edu>
2024-06-08 14:39:07 -0700

    Included ability to add fog to the scene

--- WorkShowcaseApp/src/routes/final/src/World/components/scene.js
+++ WorkShowcaseApp/src/routes/final/src/World/components/scene.js
@@ -1,9 +1,16 @@
-import { Color, Scene } from 'three';
+import { Color, Scene, Fog, FogExp2 } from 'three';
 
 function createScene() {
   const scene = new Scene();
 
   scene.background = new Color('lightsteelblue');
+
+  //scene.fog = new Fog(0xcccccc, 10, 20);
+  scene.fog = new FogExp2( 0xcccccc, 0.019 );
+
+  scene.setFog = (visible) => {
+    scene.fog = visible ? new FogExp2( 0xcccccc, 0.019 ) : null;
+  };
 
   return scene;
 }
