2d40b87c4e4814d497d2b8c145cbc720fb0e55aa
Jesus Larios <jlarios20@mail.wou.edu>
2024-06-09 19:25:17 -0700

    Added collision detection for outer plane boundry

--- WorkShowcaseApp/src/routes/final/src/World/components/car/setupModel.js
+++ WorkShowcaseApp/src/routes/final/src/World/components/car/setupModel.js
@@ -25,15 +25,27 @@ function setupModel(data, camera) {
     const targetCameraQuaternion = new Quaternion();
     const lerpFactor = 0.1;
 
+    
     model.tick = (delta) => {
         const forwardDirection = new Vector3(1, 0, 0).applyQuaternion(model.quaternion);
+        const potentialPosition = model.position.clone();
 
         if (movement.forward) {
-            model.position.add(forwardDirection.clone().multiplyScalar(-speed * delta));
+            potentialPosition.add(forwardDirection.clone().multiplyScalar(-speed * delta));
         }
         if (movement.backward) {
-            model.position.add(forwardDirection.clone().multiplyScalar(speed * delta));
+            potentialPosition.add(forwardDirection.clone().multiplyScalar(speed * delta));
         }
+
+        // Check for collisions with the plane boundaries
+        if (potentialPosition.x > 49.5) potentialPosition.x = 49.5;
+        if (potentialPosition.x < -49.5) potentialPosition.x = -49.5;
+        if (potentialPosition.z > 49.5) potentialPosition.z = 49.5;
+        if (potentialPosition.z < -49.5) potentialPosition.z = -49.5;
+    
+        // Update the model position if no collisions detected
+        model.position.copy(potentialPosition);
+
         if (movement.forward || movement.backward) {
             if (movement.left) {
                 model.rotation.z += turnSpeed * delta;
@@ -58,7 +70,7 @@ function setupModel(data, camera) {
         currentCameraQuaternion.slerp(targetCameraQuaternion, lerpFactor);
         camera.quaternion.copy(currentCameraQuaternion);
     };
-
+    
     model.move = (direction, state) => {
         movement[direction] = state;
     };
