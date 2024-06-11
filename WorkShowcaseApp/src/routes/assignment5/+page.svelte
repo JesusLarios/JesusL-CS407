353af58b9892c89497e9098f3a1196b12b0cf41d
Jesus Larios <jlarios20@mail.wou.edu>
2024-06-10 18:23:32 -0700

    Added more description about mouse controls

--- WorkShowcaseApp/src/routes/assignment5/+page.svelte
+++ WorkShowcaseApp/src/routes/assignment5/+page.svelte
@@ -79,7 +79,7 @@
           <li>Implement orbit and optionally camera navigation through the scene</li>
         </ul>
 
-        <p>For the axes helper, red represents the x-axis, green represents the y-axis, and blue represents the z-axis. You can control the camera using your mouse.</p>
+        <p>For the axes helper, red represents the x-axis, green represents the y-axis, and blue represents the z-axis. You can control the camera using your mouse (Left and right click to move around. Scroll wheel to zoom in and out.).</p>
       </div>
   </div>
 </div>
